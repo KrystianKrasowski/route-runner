d8c1dd99528675f135110619490ffac890670fa9
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2025-02-16 11:42:25 +0100

    tim3 pwm implementation

--- src/drivers/peripherals/tim3.c
+++ src/drivers/peripherals/tim3.c
@@ -1,38 +1,138 @@
 #include "tim3.h"
 #include <stm32f3xx.h>
 
+#define PWM_MODE                  6
+#define PWM_CENTER_ALIGNED_MODE_1 1
+
+static inline uint8_t
+trim_duty_cycle(uint8_t percentage);
+
 void
-tim3_ch1_init(void)
+tim3_init(void)
 {
-    // set clock access to TIM3
+    // enable clock access to TIM3
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 
-    // set compare match every 16ms on channel 1
-    TIM3->PSC  = 8 - 1;
-    TIM3->ARR  = 16000 - 1;
-    TIM3->CCR1 = 16000;
+    // set prescaler value for ~60Hz PWM frequency
+    TIM3->PSC = 665;
+    TIM3->ARR = 100 - 1;
 
-    // enable the timer
-    TIM3->CR1 |= TIM_CR1_CEN;
+    // set center aligned mode
+    // TODO: review this setting, probably mode 3 is better, but needs different
+    // timer frequency setting
+    TIM3->CR1 |= (PWM_CENTER_ALIGNED_MODE_1 << TIM_CR1_CMS_Pos);
 
-    // enable capture/compare interrupt for channel 1
-    TIM3->DIER |= TIM_DIER_CC1IE;
-
-    // enable TIM3 IRQ in NVIC
-    NVIC_EnableIRQ(TIM3_IRQn);
+    // Ensure main output is enabled
+    TIM3->BDTR |= TIM_BDTR_MOE;
 }
 
 void
-TIM3_IRQHandler(void)
+tim3_enable(void)
 {
-    if (TIM3->SR & TIM_SR_CC1IF)
-    {
-        TIM3->SR &= ~TIM_SR_CC1IF;
-        tim3_ch1_compare_isr();
-    }
+    TIM3->CR1 |= TIM_CR1_CEN;
 }
 
-__attribute__((weak)) void
-tim3_ch1_compare_isr(void)
+void
+tim3_ch3_pwm_init(void)
 {
+    // enable clock access to GPIOB
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+
+    // set PB0 in alternate function mode
+    GPIOB->MODER &= ~GPIO_MODER_MODER0_0;
+    GPIOB->MODER |= GPIO_MODER_MODER0_1;
+
+    // set AF6 for PB0
+    GPIOB->AFR[0] |= (6 << GPIO_AFRL_AFRL0_Pos);
+
+    // default duty cycle to 80%
+    TIM3->CCR3 = 80;
+
+    // set PWM mode
+    TIM3->CCMR2 |= (PWM_MODE << TIM_CCMR2_OC3M_Pos);
+
+    // set preload enable
+    // TODO: review this setting why it is neccessary
+    TIM3->CCMR2 |= TIM_CCMR2_OC3PE;
+}
+
+void
+tim3_ch3_pwm_set_duty_cycle(uint8_t percentage)
+{
+    TIM3->CCR3 = trim_duty_cycle(percentage);
+}
+
+void
+tim3_ch3_pwm_run(void)
+{
+    // Enable the output channel
+    TIM3->CCER |= TIM_CCER_CC3E;
+}
+
+void
+tim3_ch3_pwm_stop(void)
+{
+    // Disable the output channel
+    TIM3->CCER &= ~TIM_CCER_CC3E;
+}
+
+void
+tim3_ch4_pwm_init(void)
+{
+    // enable clock access to GPIOB
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+
+    // set PB1 in alternate function mode
+    GPIOB->MODER &= ~GPIO_MODER_MODER1_0;
+    GPIOB->MODER |= GPIO_MODER_MODER1_1;
+
+    // set AF6 for PB1
+    GPIOB->AFR[0] |= (6 << GPIO_AFRL_AFRL1_Pos);
+
+    // default duty cycle to 80%
+    TIM3->CCR4 = 80;
+
+    // set PWM mode
+    TIM3->CCMR2 |= (PWM_MODE << TIM_CCMR2_OC4M_Pos);
+
+    // set preload enable
+    // TODO: review this setting why it is neccessary
+    TIM3->CCMR2 |= TIM_CCMR2_OC4PE;
+}
+
+void
+tim3_ch4_pwm_set_duty_cycle(uint8_t percentage)
+{
+    TIM3->CCR4 = trim_duty_cycle(percentage);
+}
+
+void
+tim3_ch4_pwm_run(void)
+{
+    // Enable the output channel
+    TIM3->CCER |= TIM_CCER_CC4E;
+}
+
+void
+tim3_ch4_pwm_stop(void)
+{
+    // Disable the output channel
+    TIM3->CCER &= ~TIM_CCER_CC4E;
+}
+
+static inline uint8_t
+trim_duty_cycle(uint8_t percentage)
+{
+    if (percentage < 50)
+    {
+        return 50;
+    }
+    else if (percentage > 95)
+    {
+        return 95;
+    }
+    else
+    {
+        return percentage;
+    }
 }
--- src/drivers/peripherals/tim3.h
+++ src/drivers/peripherals/tim3.h
@@ -1,10 +1,36 @@
 #ifndef _TIM3_H
 #define _TIM3_H
 
-void
-tim3_ch1_init(void);
+#include <stdint.h>
 
 void
-tim3_ch1_compare_isr(void) __attribute__((weak));
+tim3_init(void);
+
+void
+tim3_enable(void);
+
+void
+tim3_ch3_pwm_init(void);
+
+void
+tim3_ch3_pwm_set_duty_cycle(uint8_t percentage);
+
+void
+tim3_ch3_pwm_run(void);
+
+void
+tim3_ch3_pwm_stop(void);
+
+void
+tim3_ch4_pwm_init(void);
+
+void
+tim3_ch4_pwm_set_duty_cycle(uint8_t percentage);
+
+void
+tim3_ch4_pwm_run(void);
+
+void
+tim3_ch4_pwm_stop(void);
 
 #endif
