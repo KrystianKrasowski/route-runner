b70eb3358ba7dc18a6b691bc7f7cf23a50fbfe0a
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2025-03-15 15:18:04 +0100

    change error range weigths

--- test/core/core_vehicle_test.c
+++ test/core/core_vehicle_test.c
@@ -2,6 +2,9 @@
 #include <unity.h>
 #include <unity_config.h>
 
+#define ON_THE_LINE  (CORE_POSITION_DETECTION_TRESHOLD)
+#define OFF_THE_LINE (CORE_POSITION_DETECTION_TRESHOLD - 1)
+
 void
 setUp(void)
 {
@@ -123,7 +126,7 @@ should_compute_position_error(uint8_t left_3,
                               uint8_t right_1,
                               uint8_t right_2,
                               uint8_t right_3,
-                              int16_t  expected_error)
+                              int16_t expected_error)
 {
     // given
     core_vehicle_t vehicle;
@@ -149,34 +152,34 @@ main(void)
     RUN_TEST(should_detect_state_change_on_transition);
     RUN_TEST(should_detect_state_change_without_transition);
     RUN_TEST(should_init_starting_position);
-    RUN_PARAM_TEST(should_detect_line, 10, 0, 0, 0, 0, 0, true);
-    RUN_PARAM_TEST(should_detect_line, 0, 10, 0, 0, 0, 0, true);
-    RUN_PARAM_TEST(should_detect_line, 0, 0, 10, 0, 0, 0, true);
-    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 10, 0, 0, true);
-    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 0, 10, 0, true);
-    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 0, 0, 10, true);
-    RUN_PARAM_TEST(should_detect_line, 9, 0, 0, 0, 0, 0, false);
-    RUN_PARAM_TEST(should_detect_line, 0, 9, 0, 0, 0, 0, false);
-    RUN_PARAM_TEST(should_detect_line, 0, 0, 9, 0, 0, 0, false);
-    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 9, 0, 0, false);
-    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 0, 9, 0, false);
-    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 0, 0, 9, false);
+    RUN_PARAM_TEST(should_detect_line, ON_THE_LINE, 0, 0, 0, 0, 0, true);
+    RUN_PARAM_TEST(should_detect_line, 0, ON_THE_LINE, 0, 0, 0, 0, true);
+    RUN_PARAM_TEST(should_detect_line, 0, 0, ON_THE_LINE, 0, 0, 0, true);
+    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, ON_THE_LINE, 0, 0, true);
+    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 0, ON_THE_LINE, 0, true);
+    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 0, 0, ON_THE_LINE, true);
+    RUN_PARAM_TEST(should_detect_line, OFF_THE_LINE, 0, 0, 0, 0, 0, false);
+    RUN_PARAM_TEST(should_detect_line, 0, OFF_THE_LINE, 0, 0, 0, 0, false);
+    RUN_PARAM_TEST(should_detect_line, 0, 0, OFF_THE_LINE, 0, 0, 0, false);
+    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, OFF_THE_LINE, 0, 0, false);
+    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 0, OFF_THE_LINE, 0, false);
+    RUN_PARAM_TEST(should_detect_line, 0, 0, 0, 0, 0, OFF_THE_LINE, false);
     RUN_PARAM_TEST(should_compute_position_error, 0, 0, 100, 100, 0, 0, 0);
-    RUN_PARAM_TEST(should_compute_position_error, 0, 50, 100, 100, 0, 0, -10);
-    RUN_PARAM_TEST(should_compute_position_error, 0, 60, 100, 90, 0, 0, -12);
-    RUN_PARAM_TEST(should_compute_position_error, 50, 50, 100, 0, 0, 0, -42);
+    RUN_PARAM_TEST(should_compute_position_error, 0, 50, 100, 100, 0, 0, -8);
+    RUN_PARAM_TEST(should_compute_position_error, 0, 60, 100, 90, 0, 0, -10);
+    RUN_PARAM_TEST(should_compute_position_error, 50, 50, 100, 0, 0, 0, -45);
     RUN_PARAM_TEST(should_compute_position_error, 100, 0, 0, 0, 0, 0, -100);
     RUN_PARAM_TEST(should_compute_position_error, 70, 0, 0, 0, 0, 0, -100);
-    RUN_PARAM_TEST(should_compute_position_error, 70, 20, 0, 0, 0, 0, -88);
+    RUN_PARAM_TEST(should_compute_position_error, 70, 20, 0, 0, 0, 0, -86);
     RUN_PARAM_TEST(should_compute_position_error, 40, 0, 0, 0, 0, 0, -100);
-    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 100, 100, 50, 0, 10);
-    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 90, 100, 60, 0, 12);
-    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 100, 50, 50, 42);
+    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 100, 100, 50, 0, 8);
+    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 90, 100, 60, 0, 10);
+    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 100, 50, 50, 45);
     RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 0, 0, 100, 100);
     RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 0, 0, 70, 100);
     RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 0, 0, 40, 100);
-    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 50, 100, 50, 52);
-    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 0, 100, 100, 75);
-    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 0, 50, 100, 83);
+    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 50, 100, 50, 50);
+    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 0, 100, 100, 70);
+    RUN_PARAM_TEST(should_compute_position_error, 0, 0, 0, 0, 50, 100, 80);
     return UNITY_END();
 }
