4581b4ae21d905e1302055bebe0754f1d1d38a12
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2025-03-16 18:53:43 +0100

    PID regulation

--- src/core/core_vehicle_tracking_motion_apply.c
+++ src/core/core_vehicle_tracking_motion_apply.c
@@ -1,8 +1,8 @@
 #include "core_vehicle_tracking_motion_apply.h"
 
-#define KP 0.7
+#define KP 0.6
 #define KI 0
-#define KD 2.25
+#define KD 3.2
 
 static inline int8_t
 pid_regulation(core_vehicle_t *vehicle);
