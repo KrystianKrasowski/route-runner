7e6ee2b1b53b94d53dfd51398cbac91ef830ea00
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2025-04-17 17:20:28 +0200

    revoke last commit

--- src/adapters/adapter_coords_qtrhd06a.c
+++ src/adapters/adapter_coords_qtrhd06a.c
@@ -14,15 +14,15 @@ adapters_coords_init(void)
 int
 adapters_coords_parse(uint8_t const *p_byte_buffer, linebot_coords_t *coords)
 {
-    uint8_t p_raw[6];
-    qtrhd06a_parse_values(p_byte_buffer, p_raw);
+    uint8_t raw[6];
+    qtrhd06a_parse_values(p_byte_buffer, raw);
 
-    return linebot_coords_acquire(normalize(p_raw[0]),
-                                  normalize(p_raw[1]),
-                                  normalize(p_raw[2]),
-                                  normalize(p_raw[3]),
-                                  normalize(p_raw[4]),
-                                  normalize(p_raw[5]),
+    return linebot_coords_acquire(normalize(raw[0]),
+                                  normalize(raw[1]),
+                                  normalize(raw[2]),
+                                  normalize(raw[3]),
+                                  normalize(raw[4]),
+                                  normalize(raw[5]),
                                   coords);
 }
 
