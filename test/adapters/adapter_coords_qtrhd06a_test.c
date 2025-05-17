f6d144a9ccb836940244aba1a03ed23f3efba6c6
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2025-05-17 11:08:44 +0200

    add coords normalization test case

--- test/adapters/adapter_coords_qtrhd06a_test.c
+++ test/adapters/adapter_coords_qtrhd06a_test.c
@@ -23,14 +23,35 @@ void
 should_return_not_ready(bool b_ready, int expected_result)
 {
     // given
-    pathbot_coords_t h_coords;
+    pathbot_coords_t coords;
     device_qtrhd06a_mock_set_ready(DEVICE_QTRHD06A_1, b_ready);
 
     // when
-    int result = adapter_coords_qtrhd06a_read(DEVICE_QTRHD06A_1, &h_coords);
+    int result = adapter_coords_qtrhd06a_read(DEVICE_QTRHD06A_1, &coords);
 
     // then
     TEST_ASSERT_EQUAL(expected_result, result);
+}
+
+void
+should_normalize_sensor_values(void)
+{
+    // given
+    uint8_t raw_coords[6] = {20, 18, 110, 105, 15, 21};
+    device_qtrhd06a_mock_set_values(DEVICE_QTRHD06A_1, raw_coords);
+    device_qtrhd06a_mock_set_ready(DEVICE_QTRHD06A_1, true);
+
+    // when
+    pathbot_coords_t coords = {
+        .weights = PATHBOT_COORDS6_WEIGHTS,
+        .length  = 6,
+    };
+    int result = adapter_coords_qtrhd06a_read(DEVICE_QTRHD06A_1, &coords);
+
+    // then
+    uint8_t expected[6] = {0, 0, 90, 85, 0, 1};
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, coords.coords, 6);
+    TEST_ASSERT_EQUAL(RESULT_OK, result);
 }
 
 /*
@@ -46,7 +67,10 @@ main(void)
     UNITY_BEGIN();
 
     RUN_PARAM_TEST(should_return_not_ready, false, RESULT_NOT_READY);
+
     RUN_PARAM_TEST(should_return_not_ready, true, RESULT_OK);
+
+    RUN_TEST(should_normalize_sensor_values);
 
     return UNITY_END();
 }
