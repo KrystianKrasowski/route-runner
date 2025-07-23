399da3b465f3464dabcf79277565d68477799e3e
Krystian Krasowski <krystiankrasowski@gmail.com>
2025-07-23 09:57:57 +0200

    setup peripherals for ADC/QTRHD06A

--- src/hardware/stm32f3_libopencm3/data_store.hpp
+++ src/hardware/stm32f3_libopencm3/data_store.hpp
@@ -9,9 +9,12 @@ namespace hardware
 struct data_store
 {
     static const uint8_t dualshock2_buffer_length = 5;
+    static const uint8_t qtrhd06a_buffer_length   = 40;
 
     // I feel like this should be defined in dualshock2 class, but it is too
     // much of a hassle with copying C-like arrays just to make code clean.
+    // Moreover, DOD decouples data from object, so maybe this is actually
+    // good approach?
     const uint8_t p_dualshock2_request[dualshock2_buffer_length] = {
         0x1, 0x42, 0x0, 0x0, 0x0
     };
@@ -19,6 +22,8 @@ struct data_store
     volatile uint8_t p_dualshock2_wbuff[dualshock2_buffer_length];
 
     volatile uint8_t p_dualshock2_rbuff[dualshock2_buffer_length];
+
+    volatile uint8_t p_qtrhd06a_wbuff[qtrhd06a_buffer_length];
 
     void
     on_dualshock2_rx_isr();
--- src/hardware/stm32f3_libopencm3/peripherals.cpp
+++ src/hardware/stm32f3_libopencm3/peripherals.cpp
@@ -1,5 +1,7 @@
 #include "peripherals.hpp"
 #include <cstdint>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/adc.h>
 #include <libopencm3/stm32/dma.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
@@ -13,6 +15,9 @@ static inline void
 rcc_setup();
 
 static inline void
+systick_setup();
+
+static inline void
 gpio_setup();
 
 static inline void
@@ -22,10 +27,16 @@ static inline void
 tim3_setup();
 
 static inline void
+tim6_setup();
+
+static inline void
 tim7_setup();
 
 static inline void
 spi_setup();
+
+static inline void
+dma1_channel1_setup(uint32_t memory_address, uint16_t memory_length);
 
 static inline void
 dma1_channel2_setup(uint32_t memory_address);
@@ -33,19 +44,45 @@ dma1_channel2_setup(uint32_t memory_address);
 static inline void
 dma1_channel3_setup(uint32_t memory_address);
 
+static inline void
+adc12_setup();
+
+static void
+systick_delay_us(uint16_t delay)
+{
+    systick_clear();
+    systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+    systick_counter_enable();
+
+    for (uint16_t i = 0; i < delay; i++)
+    {
+        while ((systick_get_countflag()) == 0)
+            ;
+    }
+
+    systick_counter_disable();
+}
+
 void
 peripherals_setup(data_store& store)
 {
     rcc_setup();
+    systick_setup();
     gpio_setup();
     tim2_setup();
     tim3_setup();
+    tim6_setup();
     tim7_setup();
     spi_setup();
+    dma1_channel1_setup(
+        (uint32_t)store.p_qtrhd06a_wbuff, store.qtrhd06a_buffer_length
+    );
     dma1_channel2_setup((uint32_t)store.p_dualshock2_wbuff);
     dma1_channel3_setup((uint32_t)store.p_dualshock2_request);
+    adc12_setup();
 }
 
+// TODO: Global system clock should be passed from the single source of truth
 static inline void
 rcc_setup()
 {
@@ -73,6 +110,16 @@ rcc_setup()
     rcc_periph_clock_enable(RCC_TIM7);
     rcc_periph_clock_enable(RCC_SPI1);
     rcc_periph_clock_enable(RCC_DMA1);
+    rcc_periph_clock_enable(RCC_ADC12);
+    rcc_adc_prescale(
+        RCC_CFGR2_ADCxPRES_PLL_CLK_DIV_2, RCC_CFGR2_ADCxPRES_PLL_CLK_DIV_1
+    );
+}
+
+static inline void
+systick_setup()
+{
+    systick_set_frequency(1000000, 16000000);
 }
 
 static inline void
@@ -104,11 +151,21 @@ gpio_setup()
     gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO6);
     gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO7);
 
-    // output compare pins for tim3 pwm signal
+    // output compare pin for tim3 channel 3
     gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN, GPIO0);
     gpio_set_af(GPIOB, GPIO_AF2, GPIO0);
+
+    // output compare pin for tim3 channel 4
     gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN, GPIO1);
     gpio_set_af(GPIOB, GPIO_AF2, GPIO1);
+
+    // QTR-HD-6A analog inputs
+    gpio_mode_setup(
+        GPIOA,
+        GPIO_MODE_ANALOG,
+        GPIO_PUPD_NONE,
+        GPIO1 | GPIO3 | GPIO4 | GPIO5 | GPIO6 | GPIO7
+    );
 }
 
 static inline void
@@ -159,6 +216,20 @@ tim3_setup()
 }
 
 static inline void
+tim6_setup()
+{
+    // set timer update to once per 700us
+    timer_set_prescaler(TIM6, 16 - 1);
+    timer_set_period(TIM6, 700 - 1);
+
+    // enable TRGO on update event
+    timer_set_master_mode(TIM6, TIM_CR2_MMS_UPDATE);
+
+    // enable timer
+    timer_enable_counter(TIM6);
+}
+
+static inline void
 tim7_setup()
 {
     // set timer frequency to 1kHz
@@ -189,6 +260,24 @@ spi_setup()
 }
 
 static inline void
+dma1_channel1_setup(uint32_t memory_address, uint16_t memory_length)
+{
+    uint32_t periph_addr = (uint32_t)&ADC12_CDR;
+
+    dma_disable_channel(DMA1, DMA_CHANNEL1);
+    dma_set_peripheral_address(DMA1, DMA_CHANNEL1, periph_addr);
+    dma_set_memory_address(DMA1, DMA_CHANNEL1, memory_address);
+    dma_set_number_of_data(DMA1, DMA_CHANNEL1, memory_length);
+    dma_set_read_from_peripheral(DMA1, DMA_CHANNEL1);
+    dma_enable_memory_increment_mode(DMA1, DMA_CHANNEL1);
+    dma_enable_circular_mode(DMA1, DMA_CHANNEL1);
+    dma_set_peripheral_size(DMA1, DMA_CHANNEL1, DMA_CCR_PSIZE_32BIT);
+    dma_set_memory_size(DMA1, DMA_CHANNEL1, DMA_CCR_MSIZE_16BIT);
+    dma_enable_transfer_complete_interrupt(DMA1, DMA_CHANNEL1);
+    dma_enable_channel(DMA1, DMA_CHANNEL1);
+}
+
+static inline void
 dma1_channel2_setup(uint32_t memory_address)
 {
     uint32_t periph_addr = (uint32_t)&SPI_DR(SPI1);
@@ -215,6 +304,45 @@ dma1_channel3_setup(uint32_t memory_address)
     dma_enable_memory_increment_mode(DMA1, DMA_CHANNEL3);
     dma_set_peripheral_size(DMA1, DMA_CHANNEL3, DMA_CCR_PSIZE_8BIT);
     dma_set_memory_size(DMA1, DMA_CHANNEL3, DMA_CCR_MSIZE_8BIT);
+}
+
+static inline void
+adc12_setup()
+{
+    // enable voltage regulators
+    adc_enable_regulator(ADC1);
+    adc_enable_regulator(ADC2);
+    systick_delay_us(10);
+
+    adc_set_multi_mode(ADC1, ADC_CCR_DUAL_REGULAR_SIMUL);
+
+    // enable DMA for dual mode 8-bit
+    ADC12_CCR |= ADC_CCR_MDMA_8_6_BIT;
+
+    // enable DMA circular
+    ADC12_CCR |= ADC_CCR_DMACFG;
+
+    adc_set_resolution(ADC1, ADC_CFGR1_RES_8_BIT);
+    adc_set_resolution(ADC2, ADC_CFGR1_RES_8_BIT);
+
+    const uint8_t sequence_length = 4;
+
+    // adc1 seqeuence needs only 2 channels, 3 and 4 are just dummy values to
+    // keep in sync with channel 2
+    uint8_t adc1_sequence[sequence_length] = {2, 4, 4, 4};
+    uint8_t adc2_sequence[sequence_length] = {1, 2, 3, 4};
+
+    adc_set_regular_sequence(ADC1, sequence_length, adc1_sequence);
+    adc_set_regular_sequence(ADC2, sequence_length, adc2_sequence);
+
+    adc_enable_external_trigger_regular(
+        ADC1, ADC_CFGR1_EXTSEL_VAL(13), ADC_CFGR1_EXTEN_RISING_EDGE
+    );
+
+    adc_power_on(ADC1);
+    adc_power_on(ADC2);
+
+    adc_start_conversion_regular(ADC1);
 }
 
 } // namespace hardware
