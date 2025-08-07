014bdf89c153351f19f4cd2ef899bd3f78ecc4fc
Krystian Krasowski <krystiankrasowski@gmail.com>
2025-08-07 17:19:19 +0200

    fix dma channel assignments for dualshock2

--- src/hardware/stm32f3_libopencm3/tree.cpp
+++ src/hardware/stm32f3_libopencm3/tree.cpp
@@ -35,7 +35,7 @@ tree::of(isr_event_emitter& events)
     auto& blink = hardware::toggle_sequence_gpio::of(TIM7, GPIOA, GPIO8);
 
     auto& remote_control = hardware::dualshock2::of(
-        GPIOF, GPIO0, DMA1, DMA_CHANNEL2, DMA_CHANNEL3, store.p_dualshock2_rbuff_
+        GPIOF, GPIO0, DMA1, DMA_CHANNEL3, DMA_CHANNEL2, store.p_dualshock2_rbuff_
     );
 
     auto& motor_left =
