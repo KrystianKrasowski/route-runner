5448a7390a8b156903386935def2f3c020e61eb2
Krystian Krasowski <krystiankrasowski@gmail.com>
2025-08-15 14:25:25 +0200

    fix test build

--- tests/linebot/api_fixture.hpp
+++ tests/linebot/api_fixture.hpp
@@ -3,7 +3,6 @@
 #include "linebot/api.hpp"
 #include "linebot/data_store.hpp"
 #include "motion_port_mock.hpp"
-#include "printer_port_mock.hpp"
 #include "route_guard_port_mock.hpp"
 #include "status_indicator_port_mock.hpp"
 
@@ -16,14 +15,11 @@ struct api_fixture
     motion_port_mock           motion_;
     status_indicator_port_mock status_indicator_;
     route_guard_port_mock      route_guard_;
-    printer_port_mock          printer_;
     api                        api_;
 
     api_fixture()
         : store_{data_store::of()},
-          api_{api::of(
-              store_, motion_, status_indicator_, route_guard_, printer_
-          )}
+          api_{api::of(store_, motion_, status_indicator_, route_guard_)}
     {
     }
 };
